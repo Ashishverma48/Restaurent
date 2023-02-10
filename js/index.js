ff1b22172fc40eec7f9e1d666b86db8273863310
Ashish Kumar Verma <117068936+Ashishverma48@users.noreply.github.com>
2023-02-11 00:26:52 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,3 +3,20 @@ const navMenu = document.getElementById('nav-menu');
 menuBar.addEventListener('click',()=>{
     navMenu.classList.toggle('active')
 })
+
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 50){
+        nav.classList.add('scroll');
+        // nav.style.backgroundColor='red';
+    }else{
+       nav.classList.remove('scroll')
+       nav.style.scrollBehavior='smooth'
+    }
+});
+// nav.addEventListener('click',()=>{
+//     nav.style.backgroundColor='red';
+    
+// })
+// console.log(nav);
