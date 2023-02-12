9cdb5467302f16669e0ba71a993b903d6494c6ca
Ashish Kumar Verma <117068936+Ashishverma48@users.noreply.github.com>
2023-02-12 12:16:48 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,22 +1,25 @@
-const menuBar = document.getElementById('menu-bar');
-const navMenu = document.getElementById('nav-menu');
-menuBar.addEventListener('click',()=>{
-    navMenu.classList.toggle('active')
-})
-
-const nav = document.getElementById('nav');
-
-window.addEventListener('scroll',()=>{
-    if(window.scrollY > 50){
-        nav.classList.add('scroll');
-        // nav.style.backgroundColor='red';
-    }else{
-       nav.classList.remove('scroll')
-       nav.style.scrollBehavior='smooth'
-    }
+const menuBar = document.getElementById("menu-bar");
+const navMenu = document.getElementById("nav-menu");
+menuBar.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
 });
-// nav.addEventListener('click',()=>{
-//     nav.style.backgroundColor='red';
-    
-// })
-// console.log(nav);
+
+const nav = document.getElementById("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    nav.classList.add("scroll");
+  } else {
+    nav.classList.remove("scroll");
+    nav.style.scrollBehavior = "smooth";
+  }
+});
+
+const home = document.getElementById("home-back");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+      home.style.display='flex'
+} else {
+    home.style.display='none'
+  }
+});
